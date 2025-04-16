02058220838e59c645a684adf189f662fab112d6
RykerMcKnight <139500274+PiRenk1028@users.noreply.github.com>
2025-04-16 11:33:48 -0600

    finished queries

--- queries.sql
+++ queries.sql
@@ -4,7 +4,18 @@ SELECT *
 FROM F1;
 
 -- Ryker Queries 
+SELECT Team_Name, Car_Name, Car_Primary_Color, Car_Secondary_Color
+FROM teams
+JOIN cars ON teams.Car_ID = cars.Car_ID
+ORDER BY Team_ID;
 
+SELECT CONCAT(c1.Car_Name, ' and ', c2.Car_Name) as "Matching Car Names", c1.Car_Primary_Color as "Primary Color"
+FROM cars c1
+JOIN cars c2 ON c1.Car_Primary_Color = c2.Car_Primary_Color AND c1.Car_ID != c2.Car_ID
+LIMIT 3;
+
+SELECT CAST(AVG(DRIVER_SALARY_TOTALS) AS DECIMAL(10,2)) AS "Average Driver Salary Totals"
+FROM finances;
 
 -- Louis Query 
 SELECT Race_Number, Name, Total_Points

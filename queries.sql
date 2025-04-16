bb455acda8aa94f6d792f6351def921b859c9e91
Louis <159062608+LouisNe@users.noreply.github.com>
2025-04-16 11:19:15 -0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -2,3 +2,14 @@ USE F1;
 
 SELECT *
 FROM F1;
+
+-- Ryker Queries 
+
+
+-- Louis Query 
+SELECT Race_Number, Name, Total_Points
+FROM Drivers 
+WHERE NOT Total_Points = 0.0
+Order By Total_Points DESC;
+
+-- Aylssa Query 
